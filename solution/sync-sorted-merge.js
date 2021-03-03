7ba7ba0b19b0ad8ba51bb0e28433bdf26717b524
Sergio Baquero <sbaqueroa@gmail.com>
2021-03-03 16:43:30 -0500

    Sync order initial commit.

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,20 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  //Pop all entries and store them in a list
+  let headEntries = logSources.map( (source) => source.pop())
+  while(headEntries.filter(entry => entry).length > 0){
+    //order entry list
+    let orderedList = headEntries.sort((x, y) => {
+      if(!x && !y) return 0
+      if(!y) return -1
+      if(!x) return 1
+      return x.date.getTime() - y.date.getTime()
+    })
+    //print log 
+    printer.print(orderedList[0])
+    //pop log from source where log was printed.
+    let index = headEntries.findIndex(item => item === orderedList[0])
+    headEntries[index] = logSources[index].pop()
+  }
 };
