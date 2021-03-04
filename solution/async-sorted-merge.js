8f014255518a51ed8a815663580b51c184d6e306
Sergio Baquero <sbaqueroa@gmail.com>
2021-03-04 10:00:48 -0500

    Data structure to manage logSources order was added.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const logUtils = require("./utils/log-utils.js")
+const SortedMap = require("collections/sorted-map");
 const orderEntriesByDate = logUtils.orderEntriesByDate
 
 // Print all entries, across all of the *async* sources, in chronological order.
@@ -8,15 +9,21 @@ const orderEntriesByDate = logUtils.orderEntriesByDate
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
     //Pop all entries and store them in a list
-    Promise.all(logSources.map( (source) => source.popAsync())).then( async (headEntries) => {
-      while(headEntries.filter(entry => entry).length > 0){
-        //order entry list
-        let orderedList = headEntries.sort(orderEntriesByDate)
-        //print log
-        printer.print(orderedList[0])
+    Promise.all(logSources.map( async (source) => {
+      const log = await source.popAsync()
+      return !!log? [log, source] : [null, null]
+    })).then( async (entries) => {
+      let headEntries = new SortedMap(new Map(entries), (x,y) => x === y,  orderEntriesByDate)
+      while(headEntries.size > 0){
+        //print log 
+        var entry = headEntries.store.min()
+        printer.print(entry.key)
         //pop log from source where log was printed.
-        let index = headEntries.findIndex(item => item === orderedList[0])
-        headEntries[index] = await logSources[index].pop()
+        headEntries.delete(entry.key)
+        const key = await entry.value.pop()
+        if(key){
+          headEntries.set(key, entry.value)
+        }
       }
       resolve(console.log("Async sort complete."));
     })
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,19 +3,26 @@
 // Print all entries, across all of the sources, in chronological order.
 
 const logUtils = require("./utils/log-utils.js")
+const SortedMap = require("collections/sorted-map");
 const orderEntriesByDate = logUtils.orderEntriesByDate
 
 module.exports = (logSources, printer) => {
   //Pop all entries and store them in a list
-  let headEntries = logSources.map( (source) => source.pop())
-  while(headEntries.filter(entry => entry).length > 0){
-    //order entry list
-    let orderedList = headEntries.sort(orderEntriesByDate)
+  const entries = logSources.map( (source) => {
+    const log = source.pop()
+    return !!log? [log, source] : [null, null]
+  })
+  let headEntries = new SortedMap(new Map(entries), (x,y) => x === y,  orderEntriesByDate)
+  while(headEntries.size > 0){
     //print log 
-    printer.print(orderedList[0])
+    var entry = headEntries.store.min()
+    printer.print(entry.key)
     //pop log from source where log was printed.
-    let index = headEntries.findIndex(item => item === orderedList[0])
-    headEntries[index] = logSources[index].pop()
+    headEntries.delete(entry.key)
+    const key = entry.value.pop()
+    if(!!key){
+      headEntries.set(key, entry.value)
+    }
   }
   return console.log("Sync sort complete.");
 };
